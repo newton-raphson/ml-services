a5f222dd380e9f7d19e94ec34b117484af1e4b8b
newton-raphson <newton@mokshya.io>
2023-04-15 18:27:06 +0545

    ml app

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.endpoints',
+    'apps.ml',
+     'rest_framework'
 ]
 
 MIDDLEWARE = [
--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# backend/server/server/urls.py file
+from django.urls import path, re_path
 from django.contrib import admin
-from django.urls import path
+
+from apps.endpoints.urls import urlpatterns as endpoints_urlpatterns
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path('admin/', admin.site.urls),
 ]
+
+urlpatterns += endpoints_urlpatterns
